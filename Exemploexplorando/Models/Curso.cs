8773695f84b4a2b6b40adcaaef552d46a145631c
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-07 21:04:29 -0300

    Update Curso C#

--- Exemploexplorando/Models/Curso.cs
+++ Exemploexplorando/Models/Curso.cs
@@ -6,6 +6,25 @@ using System.Security.Cryptography.X509Certificates;
 
     public void AdicionarAluno(Pssoa aluno)
     {
-        Aluno.add(aluno);
+        Alunos.Add(aluno);
+    }
+    public int obterQuantidadeDeAlunoMatriculado()
+    {
+        int quantidade = Alunos.Count;
+        return quantidade;
+    }
+
+    public bool RemoverAluno(Pessoa aluno)
+    {
+        return Alunos.Remove(aluno);
+    }
+
+    public void ListarAlunos()
+    {
+        Console.writeLine($"Alunos do curso de: {Nome}");
+        foreach (Pessoa aluno in Alunos)
+        {
+            Console.WriteLine(aluno.NomeCompleto);
+        }
     }
 }
