3c86527e705ada0648d7c25ea1cc0042275f2593
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-10 07:22:29 -0300

    Update Curso.cs

--- Exemploexplorando/Models/Curso.cs
+++ Exemploexplorando/Models/Curso.cs
@@ -1,30 +1,49 @@
-using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace EXEMPLOEXPLORANDO.Models
 {
-    Public string Nome {get; set;}
-    Public List<Pesssoa> Alunos { get; set; }
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
 
-    public void AdicionarAluno(Pssoa aluno)
-    {
-        Alunos.Add(aluno);
-    }
-    public int obterQuantidadeDeAlunoMatriculado()
-    {
-        int quantidade = Alunos.Count;
-        return quantidade;
-    }
-
-    public bool RemoverAluno(Pessoa aluno)
-    {
-        return Alunos.Remove(aluno);
-    }
-
-    public void ListarAlunos()
-    {
-        Console.writeLine($"Alunos do curso de: {Nome}");
-        foreach (Pessoa aluno in Alunos)
+        public void AdicionarAluno(Pessoa aluno)
         {
-            Console.WriteLine(aluno.NomeCompleto);
+            Alunos.Add(aluno);
+        }
+
+        public int ObterQuantidadeDeAlunosMatriculados()
+        {
+            int quantidade = Alunos.Count;
+            return quantidade;
+        }
+
+        public bool RemoverAluno(Pessoa aluno)
+        {
+            return Alunos.Remove(aluno);
+        }
+
+        public void ListarAlunos()
+        {
+            Console.WriteLine($"Alunos do curso: {Nome}");
+          
+          for(int count =0; count < Alunos.Count; count++){
+            //Concatenação comum
+            //string texto = "Nº " + count + " - " + Alunos[count].NomeCompleto;
+
+            //Interpolação => O uso do $""
+            string texto = $"Nº {count+1} - {Alunos[count].NomeCompleto}";
+
+            Console.WriteLine(texto);
+          }
+          
+            // foreach (Pessoa aluno in Alunos)
+            // {
+            //     Console.WriteLine(aluno.NomeCompleto);
+            // }
         }
     }
 }
