3a281d7f5aeed9ddbabe3ff4611b907afd90d5f5
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-10 07:19:15 -0300

    Update Pessoa.cs

--- Exemploexplorando/Models/Pessoa.cs
+++ Exemploexplorando/Models/Pessoa.cs
@@ -1,38 +1,67 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
 
-namespace exemploexplorando.models
+namespace EXEMPLOEXPLORANDO.Models
 {
     public class Pessoa
     {
-      private string _nome;
-      public string Nome
-      { 
-          get
-          {
-            return _nome.ToUpper();
-          }
+
+        public Pessoa()
+        {
+
+        }
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
         
-          set
-          {
-             if (value == "")
-             {
-                throw new ArgumentException("O nome não pode ser vazio");
-             }
+        //criando desconstrutor
+        public void Deconstruct(out string nome, out string sobrenome)
+        {
+            nome = Nome;
+            sobrenome = Sobrenome;
+        }
 
-             _nome = value;
-          }
-      } 
+        private string _nome;
+        private int _idade;
+        public string Nome
+        {
+            get => _nome.ToUpper();
 
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            }
 
-      public int Idade { get; set; } 
+        }
 
-      public void Apresentar()
-      {
-        Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
-      }
+        public string Sobrenome { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
+        public int Idade
+        {
+            get => _idade;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("O valor nao pode ser menor do que zero");
+                }
+                _idade = value;
+            }
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"{NomeCompleto} e tenho {Idade} anos");
+        }
     }
 }
-//quando voce tem um get voce esta chamando um valor 
