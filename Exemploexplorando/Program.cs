c8afd5376ea38424457670f3bb48d88915f58b28
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-14 10:24:03 -0300

    Program.cs

--- Exemploexplorando/Program.cs
+++ Exemploexplorando/Program.cs
@@ -5,13 +5,48 @@ using System.Text.Json.Nodes;
 using Newtonsoft.Json;
 
 
-Venda v1 = new Venda(1, "material de escritorio", 25.00M);
 
-string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+//deserializando um objeto
 
-File.WriteAllText("Arquivos/vendas.json", serializado);
+string conteudoArquivo =File.ReadAllText("Arquivos/vendas.json");
 
-Console.WriteLine(serializado);
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+foreach (Venda venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id}, Prodiuto: {venda.Produto}, " + 
+                      $"Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//DateTime dataAtual = DateTime.Now;
+
+//List<Venda> listaVendas = new List<Venda>();
+
+//Venda v1 = new Venda(1, "material de escritorio", 25.00M, dataAtual);
+//Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
+
+//listaVendas.Add(v1);
+//listaVendas.Add(v2);
+
+
+//string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+
+//File.WriteAllText("Arquivos/vendas.json", serializado);
+
+//Console.WriteLine(serializado);
+
 
 
 //int numero = 20;
