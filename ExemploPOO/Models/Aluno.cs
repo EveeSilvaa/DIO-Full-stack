405290c2ff7ba725a7f0ba595fdc569db230c633
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-16 07:34:27 -0300

    Aluno.cs

---
+++ ExemploPOO/Models/Aluno.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    public class Aluno : Pessoa // herança ele puxa os atributos de outra class exemplo aluno esta puxando a herança/ class de pessoa
+    {
+       public double Nota { get; set; }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, e sou uma aluna nota {Nota}!");
+        }
+    }
+}
+//quando usa overraido voce está chamando o metodo apresentar, so que o da classe aluno 
