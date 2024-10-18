b4a1dbd94ae4ced402925b70c8ecfb33b7eacb1b
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-18 14:26:07 -0300

    Program.cs

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -1,23 +1,34 @@
 using ExemploPOO.Models;
 
 
+Conta c = new Conta();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //herança na pratrica 
-Aluno a1 = new Aluno();
-a1.Nome = "Maria";
-a1.Idade = 20;
-a1.Email = "test@test.com";
-a1.Nota = 10;
-a1.Apresentar();
+//Aluno a1 = new Aluno();
+//a1.Nome = "Maria";
+//a1.Idade = 20;
+//a1.Email = "test@test.com";
+//a1.Nota = 10;
+//a1.Apresentar();
 
-Professor p1 = new Professor();
-p1.Nome = "Eduardo";
-p1.Idade = 30;
-p1.Salario = 1000;
-p1.Apresentar();
-
-
-
-
+//Professor p1 = new Professor();
+//p1.Nome = "Eduardo";
+//p1.Idade = 30;
+//p1.Salario = 1000;
+//p1.Apresentar();
 
 
 // ContaCorrente c1 = new ContaCorrente(123, 1000);
