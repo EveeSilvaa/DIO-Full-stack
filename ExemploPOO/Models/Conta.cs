95a8079353653d5d61ea1242e2a88edce5cc49e7
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-18 14:23:17 -0300

    Conta.cs

--- ExemploPOO/Models/Conta.cs
+++ ExemploPOO/Models/Conta.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace ExemploPOO.Models
+{
+    public abstract class Conta
+    {
+      protected decimal saldo;
+
+      public abstract void Creditar(decimal valor);
+
+      public void ExibirSaldo()
+      {
+        Console.WriteLine("O seu saldo é: " + saldo); 
+      }
+    }
+}
+// o metdod protected, ele significa que está protegido contra alterações externas com exceção das suas classes
