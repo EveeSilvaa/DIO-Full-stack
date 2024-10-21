5927f40144ab8c9598ae16003fb5ee75f862b543
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-21 11:43:14 -0300

    Update Professor.cs

--- ExemploPOO/Models/Professor.cs
+++ ExemploPOO/Models/Professor.cs
@@ -5,13 +5,26 @@ using System.Threading.Tasks;
 
 namespace ExemploPOO.Models
 {
-    public class Professor : Pessoa 
-    { 
-       public decimal Salario { get; set; }
-
-        public override void Apresentar()
+    //sealed informa que classe é selada e não pode ser herdada
+    public class Professor : Pessoa
+    {
+        // public Professor()
+        // {
+           
+        // }
+        //O base(nome) informa que estamos passando o valor para a classe pai
+        //Nesse exemplo a classe pae é a classe pessoa
+        public Professor(string nome) : base(nome)
         {
-            Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos, sou um professor e ganho {Salario}");
+            //Método necessário pois classe pai exige o atributo nome no construtor
+        }
+        public decimal Salario { get; set; }
+        //A palavra override informa que estou sobreescrevendo o método da classe pai
+        //de onde foi herdado demais atributos e métodos
+        //sealed informa que metodo é selado e não pode ser sobreescrito pela classe filha
+        public sealed override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos, e meu salário é {Salario}");
         }
     }
 }
