536d6a78afd6ddbcc978b96e0292b1a22dba44e1
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-21 11:44:57 -0300

    Update Pessoa.cs

--- ExemploPOO/Models/Pessoa.cs
+++ ExemploPOO/Models/Pessoa.cs
@@ -7,10 +7,20 @@ namespace ExemploPOO.Models
 {
     public class Pessoa
     {
+        // public Pessoa()
+        // {
+
+        // }
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
         public string Nome { get; set; }
         public int Idade { get; set; }
         public string Email { get; set; }
 
+        //A palavra Virtual informa que esse método pode ser sobrescrito
+        //pela clase filha que está herdando dessa classe
         public virtual void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos!");
