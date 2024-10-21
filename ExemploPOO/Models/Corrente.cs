33637f1a4b7a5905fa77fcdd8f9ad640e83d6988
Maria Evellyn da silva <evellynsilvadeveloper@gmail.com>
2024-10-21 11:46:00 -0300

    Update Corrente.cs

--- ExemploPOO/Models/Corrente.cs
+++ ExemploPOO/Models/Corrente.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace ExemploPOO.Models
+{
+    public class Corrente : Conta
+    {
+        //A palavra override informa que estou sobreescrevendo o método da classe pai
+        //de onde foi herdado demais atributos e métodos
+        public override void Creditar(decimal valor)
+        {
+            Saldo += valor;
+        }
+    }
+}
